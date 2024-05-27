5af1d31ab12e835042483ab2c8eae50199a87a4a
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-05-27 21:01:20 +0400

    ui fixing

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -5,6 +5,7 @@ import {
   Table,
   TableBody,
   TableContainer,
+  TextField,
   Typography,
 } from "@mui/material";
 import Row from "./row";
@@ -34,16 +35,73 @@ const Calculator = () => {
           display: "flex",
           backgroundColor: "#282c34",
           flexDirection: "column",
-          backgroundImage: "url(https://as1.ftcdn.net/v2/jpg/05/74/79/80/1000_F_574798026_iEIdURVR3yieUYcn2tQYakrSYB999s5k.jpg)",
-          backgroundSize: "cover", 
-          backgroundPosition: "center", 
+          backgroundImage:
+            "url(https://as1.ftcdn.net/v2/jpg/05/74/79/80/1000_F_574798026_iEIdURVR3yieUYcn2tQYakrSYB999s5k.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
-        <Box sx={{ color: "white", height: "100px",backgroundColor: "#0e0a0aac", }}>
-          <Typography gutterBottom component="div">
-            Balance: 10000
-          </Typography>
-          <Button variant="contained" onClick={handleAddRow}>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            color: "white",
+            alignItems: "center",
+            minHeight: "50px",
+            height: "max-content",
+            backgroundColor: "#000000ac",
+          }}
+        >
+          <Box
+            sx={{
+              color: "white",
+              display: "flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              gap: "20px",
+              padding: "20px",
+            }}
+          >
+            <TextField
+              id="C4"
+              label="Balance"
+              value={"52525"}
+              // onChange={() => {}}
+              size="small"
+              className="balance"
+            />
+            <TextField
+              id="C4"
+              label="Initial Balance"
+              // value={""}
+              // onChange={() => {}}
+              className="balance"
+              size="small"
+            />
+            <Typography gutterBottom component="div">
+              Accumulated Balance <Button size="small">o</Button>
+            </Typography>
+            <Typography gutterBottom component="div" style={{ color: "green" }}>
+              Number of Longs{" "}
+              <Button variant="contained" color="success" size="small">
+                0
+              </Button>
+            </Typography>
+            <Typography gutterBottom component="div" style={{ color: "red" }}>
+              Number of Shorts{" "}
+              <Button variant="contained" color="error" size="small">
+                0
+              </Button>
+            </Typography>
+            <Typography gutterBottom component="div">
+              Coin Quantity <Button size="small">0</Button>
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            onClick={handleAddRow}
+            style={{ maxHeight: "50px" }}
+          >
             Add Row
           </Button>
         </Box>
