47601d45bbe073dab40766778ae5068fc69128c3
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-10-13 19:04:18 +0400

    e

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,15 +1,22 @@
-import React, { createContext, useState, useContext, ReactNode, useEffect } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import { v4 as uuidv4 } from "uuid";
 import { calculationResult, callAll, data } from "../helpers/clearing";
 import { useAuthContext } from "./AuthContext";
 import { getUserCoinsCalculations } from "../services/coinsCalculationsService";
 import { useCoinsCalculationsContext } from "./CoinsCalculationsContext";
+import { getGeneralDataByUserId } from "../services/generalData";
 
 export type RowData = {
   id: string;
   data: GeneralData;
   results: typeof calculationResult;
-  name: string
+  name: string;
 };
 
 export type GeneralData = {
@@ -40,12 +47,19 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     if (user && Object.keys(user).length !== 0) {
       const data = await getUserCoinsCalculations(user.userId);
       console.log(data);
-      triggerCalculations(data)
+      triggerCalculations(data);
       setRows(data);
     }
   };
 
+  const fetchGeneralData = async () => {
+    const dataDB = await getGeneralDataByUserId(user.userId);
+    setGeneralData({ ...dataDB, ...data });
+    return dataDB;
+  };
+
   useEffect(() => {
+    fetchGeneralData();
     fetchUserCoinsCalculations();
   }, [user]);
 
@@ -66,12 +80,8 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     accumulatedBalance: any,
     length: any
   ) => {
-    
-    const data = accumulatedBalance /length;
-    const price =
-      data /
-      (results.data.D4 +
-        results.data.E4);
+    const data = accumulatedBalance / length;
+    const price = data / (results.data.D4 + results.data.E4);
 
     return price;
   };
@@ -83,22 +93,21 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     return results.length > 0 ? accumulatedBalance / results.length : 0;
   };
 
-
   const addRow = () => {
     const newRow = {
       id: uuidv4(),
       data: data,
       results: { ...calculationResult },
-      name: ''
+      name: "",
     };
     setRows((prevRows) => [...prevRows, newRow]);
     setGeneralData((prevGeneralData) => {
-      return { ...prevGeneralData, 'E242': rows.length + 1 };
+      return { ...prevGeneralData, E242: rows.length + 1 };
     });
   };
 
   const deleteRow = (id: string) => {
-    deleteRowInBE(id)
+    deleteRowInBE(id);
     const newRows = rows.filter((row) => row.id !== id);
     setRows(newRows);
     triggerCalculations(newRows);
@@ -114,13 +123,18 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
 
   const triggerCalculations = (rows: RowData[]): void => {
     let longShorts = {
-      'CG4': 0,
-      'CH4': 0,
+      CG4: 0,
+      CH4: 0,
     };
-    let updatedRows:RowData[] =[];
+    let updatedRows: RowData[] = [];
     for (let i = 0; i < rows.length; ++i) {
       const row = rows[i];
-      const rowData = { ...row.data, "A242": generalData.A242, "D244": generalData.D244, 'E242': generalData['E242'] };
+      const rowData = {
+        ...row.data,
+        A242: generalData.A242,
+        D244: generalData.D244,
+        E242: generalData["E242"],
+      };
       const { calculationResults, rowBigData } = callAll(
         row.results,
         rowData,
@@ -128,17 +142,21 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
         i
       );
       for (const key in rowBigData) {
-        if (typeof rowBigData[key] === 'function') {
+        if (typeof rowBigData[key] === "function") {
           delete rowBigData[key];
         }
       }
-      
+
       longShorts["CG4"] += rowBigData["CG4"];
       longShorts["CH4"] += rowBigData["CH4"];
       updatedRows.push({
         ...row,
         results: calculationResults,
-        data: { ...rowBigData, "A242": generalData["A242"], "D244": generalData["D244"], },
+        data: {
+          ...rowBigData,
+          A242: generalData["A242"],
+          D244: generalData["D244"],
+        },
       });
     }
 
@@ -150,12 +168,15 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     }
 
     const calculateAccumulatedBalance = () => {
-      const aa =  updatedRows[updatedRows.length - 1]?.data["B242"] >= generalData["D244"]
-      ? generalData["A242"] - updatedRows[updatedRows.length - 1].data["B242"]
-      : generalData["A242"] - generalData["D244"];
+      const aa =
+        updatedRows[updatedRows.length - 1]?.data["B242"] >= generalData["D244"]
+          ? generalData["A242"] -
+            updatedRows[updatedRows.length - 1].data["B242"]
+          : generalData["A242"] - generalData["D244"];
       // debugger
-      return aa
+      return aa;
     };
+
     const accumulatedBalance = calculateAccumulatedBalance();
     const averagedRationalTradingMargin =
       calculateAveragedRationalTradingMargin(
@@ -163,22 +184,27 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
       );
     const accumulatedBalanceForPosition =
       calculateAccumulatedBalanceForPosition(updatedRows, accumulatedBalance);
-      
-      let newUpdatedRows = []
-      for (const row of updatedRows) {
-        const priceAccordingAccumulatedBalance = calculatePriceAccordingAccumulatedBalance(row,accumulatedBalance,updatedRows.length)
-        newUpdatedRows.push({
-          ...row,
-          results: {
-            ...row.results,
-            accumulatedBalance,
-            priceAccordingAccumulatedBalance,
-            accumulatedBalanceForPosition,
-            averagedRationalTradingMargin,
-          },
-        });
-      }
-  
+
+    let newUpdatedRows = [];
+    for (const row of updatedRows) {
+      const priceAccordingAccumulatedBalance =
+        calculatePriceAccordingAccumulatedBalance(
+          row,
+          accumulatedBalance,
+          updatedRows.length
+        );
+      newUpdatedRows.push({
+        ...row,
+        results: {
+          ...row.results,
+          accumulatedBalance,
+          priceAccordingAccumulatedBalance,
+          accumulatedBalanceForPosition,
+          averagedRationalTradingMargin,
+        },
+      });
+    }
+
     setGeneralData({
       ...generalData,
       E242: updatedRows.length,
@@ -187,7 +213,7 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
       accumulatedBalance:
         accumulatedBalance || generalData["A242"] - generalData["D244"],
     });
-console.log(newUpdatedRows, 'eslel karevor');
+    console.log(newUpdatedRows, "eslel karevor");
 
     setRows(newUpdatedRows);
   };
